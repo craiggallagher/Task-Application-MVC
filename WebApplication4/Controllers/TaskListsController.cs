f79b69b03352df4fac613a632b2d46ce3fb04db1
craiggallagher <donegal@live.com>
2016-09-16 17:01:13 +0100

    Task Application

---
+++ WebApplication4/Controllers/TaskListsController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class TaskListsController : Controller
+    {
+        private TasksEntities db = new TasksEntities();
+
+        // GET: TaskLists
+        public ActionResult Index()
+        {
+            return View(db.TaskLists.ToList());
+        }
+        public ActionResult TaskDetails()
+        {
+            return View ();
+        }
+
+        // GET: TaskLists/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskList taskList = db.TaskLists.Find(id);
+            if (taskList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskList);
+        }
+
+        // GET: TaskLists/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TaskLists/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TaskListId,Name")] TaskList taskList)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TaskLists.Add(taskList);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(taskList);
+        }
+
+        // GET: TaskLists/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskList taskList = db.TaskLists.Find(id);
+            if (taskList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskList);
+        }
+
+        // POST: TaskLists/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TaskListId,Name")] TaskList taskList)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(taskList).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(taskList);
+        }
+
+        // GET: TaskLists/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskList taskList = db.TaskLists.Find(id);
+            if (taskList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskList);
+        }
+
+        // POST: TaskLists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                TaskList taskList = db.TaskLists.Find(id);
+                db.TaskLists.Remove(taskList);
+                db.SaveChanges();
+                
+
+            }
+            catch
+            {
+                TempData["alertMessage"] = "Whatever you want to alert the user with";
+                return View();
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication4/Controllers/TasksController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class TasksController : Controller
+    {
+        private TasksEntities db = new TasksEntities();
+
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            var tasks = db.Tasks.Include(t => t.Task2).Include(t => t.TaskList);
+            return View(tasks.ToList());
+        }
+
+        // GET: Tasks/Details/5
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var task = db.Tasks.Find(id);
+            }
+            catch (HttpException e)
+            {
+                throw new HttpException(404, "Task not found.");
+            }
+
+            return View(db.Tasks.Find(id));
+        }
+        [HttpGet]
+        public PartialViewResult ShowTaskByID(int id)
+        {
+            try
+            {
+                var tasks = db.Tasks.ToList();
+            }
+            catch (HttpException e)
+            {
+                throw new HttpException(404, "Task nout found.");
+            }
+
+            return PartialView("_ShowTask", db.Tasks.Find(id));
+        }
+
+
+
+        // GET: Tasks/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.TaskListId = new SelectList(db.TaskLists, "TaskListId", "Name");
+            return View();
+        }
+        // POST: Tasks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TaskId,TaskListId,TaskName,StartDate,FinishDate")] Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TaskListId = new SelectList(db.Tasks, "TaskId", "TaskName", task.TaskListId);
+            ViewBag.TaskListId = new SelectList(db.TaskLists, "TaskListId", "Name", task.TaskListId);
+            return View(task);
+        }
+
+        // GET: Tasks/Edit/5
+        public PartialViewResult EditTaskID(int id)
+        {
+            return PartialView("_EditTask", db.Tasks.Find(id));
+        }
+
+        [HttpPost, ActionName("EditTaskID")]
+       public ActionResult Edit([Bind(Include = "TaskId,TaskListId,TaskName,StartDate,FinishDate")] Task editTask)
+        {
+            try
+            {
+
+                db.Entry(editTask).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TaskListId = new SelectList(db.Tasks, "TaskId", "TaskName", task.TaskListId);
+            ViewBag.TaskListId = new SelectList(db.TaskLists, "TaskListId", "Name", task.TaskListId);
+            return View(task);
+        }
+
+        // POST: Tasks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "TaskId,TaskListId,TaskName,StartDate,FinishDate")] Task task)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(task).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    ViewBag.TaskListId = new SelectList(db.Tasks, "TaskId", "TaskName", task.TaskListId);
+        //    ViewBag.TaskListId = new SelectList(db.TaskLists, "TaskListId", "Name", task.TaskListId);
+        //    return View(task);
+        //}
+        public PartialViewResult TaskByID(int id)
+        {
+            return PartialView("_DeleteTask", db.Tasks.Find(id));
+        }
+        [HttpPost, ActionName("TaskByID")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            db.Tasks.Remove(db.Tasks.Find(id));
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        // GET: Tasks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
